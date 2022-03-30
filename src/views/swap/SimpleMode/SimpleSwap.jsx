b0a1bc108198b3d24b6d4de59ffe225f5ba90da2
gamut-exchange <team@gamut.exchange>
2022-03-30 02:12:38 +0200

    adding ABI, List with selction

--- src/views/swap/SimpleMode/SimpleSwap.jsx
+++ src/views/swap/SimpleMode/SimpleSwap.jsx
@@ -14,6 +14,8 @@ import TextField from "@mui/material/TextField";
 import { AiOutlineArrowDown, AiOutlineLineChart } from "react-icons/ai";
 import { ImLoop } from "react-icons/im";
 
+import './SimpleSwap.css'
+
 const StyledModal = tw.div`
   flex
   flex-col
@@ -26,6 +28,15 @@ const StyledModal = tw.div`
   transform -translate-x-1/2 -translate-y-1/2
   w-1/3
   `;
+
+let img_in = "https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ";
+let img_out = "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe";
+
+let in_value = "BTCC"
+let out_value = "ETH"
+
+var text_in = "123"
+var text_out = "456"
 
 const SimpleSwap = () => {
   const [crypto, setCrypto] = useState("");
@@ -82,8 +93,8 @@ const SimpleSwap = () => {
               <h3 className="input-lable mb-4">Input</h3>
               <div className="flex flex-wrap sm:flex-row flex-col justify-between sm:items-center p-2 sm:p-4 rounded-sm bg-grey-dark bg-opacity-30 dark:bg-off-white dark:bg-opacity-10">
                 <div>
-                  <Button variant="outlined" startIcon={<img src={btc} alt="" />} style={{padding:'10px 15px'}} onClick={() =>handleOpen(0)}>
-                    BTC
+                  <Button id="address_in" variant="outlined" startIcon={<img src={img_in} alt="" />} style={{padding:'10px 15px'}} onClick={() =>handleOpen(0)}>
+                   {text_in}
                   </Button>
                 </div>
                 <div className="text-right">
@@ -110,8 +121,8 @@ const SimpleSwap = () => {
               <h3 className="input-lable mb-4">Output</h3>
               <div className="flex flex-wrap sm:flex-row flex-col justify-between sm:items-center p-2 sm:p-4 rounded-sm bg-grey-dark bg-opacity-30 dark:bg-off-white dark:bg-opacity-10">
                 <div>
-                  <Button variant="outlined" startIcon={<img src={eth} alt="" />} style={{padding:'10px 15px'}} onClick={() =>handleOpen(1)}>
-                    ETH
+                  <Button id="address_out" variant="outlined" startIcon={<img src={img_out} alt="abc" />} style={{padding:'10px 15px'}} onClick={() =>handleOpen(1)}>
+                    {text_out}
                   </Button>
                 </div>
                 <div className="text-right">
@@ -157,7 +168,7 @@ const SimpleSwap = () => {
                 freeSolo
                 id="free-solo-2-demo"
                 disableClearable
-                options={ethlist.map((option) => option.value)}
+                options={uniList.map((option) => option.value)}
                 className="input-value"
                 renderInput={(params) => (
                   <TextField
@@ -177,30 +188,30 @@ const SimpleSwap = () => {
               />
               <hr className="my-6" />
               {selected === 0 && 
-                <ul className="flex flex-col gap-y-6">
-                  {cryptolist.map((item) => {
-                    const { id, icon, value } = item;
+                <ul className="flex flex-col gap-y-2">
+                  {uniList.map((item) => {
+                    const { address, logoURL, symbol} = item;
                     return (
-                      <li key={id} className="flex gap-x-1">
+                      <li key={address} className="flex gap-x-1 thelist"  onClick={() => selectToken({address},symbol ,logoURL, selected)}>
                         <div className="relative flex">
-                          <img src={icon} alt="" />
+                          <img src={logoURL} alt="" />
                         </div>
-                        <p className="text-light-primary text-lg">{value}</p>
+                        <p className="text-light-primary text-lg">{symbol}</p>
                       </li>
                     );
                   })}
                 </ul>
               }
               {selected === 1 && 
-                <ul className="flex flex-col gap-y-6">
-                  {ethlist.map((item) => {
-                    const { id, icon, value } = item;
+                <ul className="flex flex-col gap-y-2">
+                  {uniList.map((item) => {
+                    const { address, logoURL, symbol} = item;
                     return (
-                      <li key={id} className="flex gap-x-1">
+                      <li key={address} className="flex gap-x-1 thelist" onClick={() => selectToken({address},symbol ,logoURL, selected)}>
                         <div className="relative flex">
-                          <img src={icon} alt="" />
+                          <img src={logoURL} alt="" />
                         </div>
-                        <p className="text-light-primary text-lg">{value}</p>
+                        <p className="text-light-primary text-lg">{symbol}</p>
                       </li>
                     );
                   })}
@@ -212,25 +223,34 @@ const SimpleSwap = () => {
       </div>
     </div>
   );
+
+  async function selectToken(address, value , url, selected){
+    handleClose()
+
+    if(selected == 0){
+      token_In = address
+      img_in = url
+      text_in = value
+      
+
+    } else if (selected == 1){
+      token_Out = address
+      img_out = url
+      text_out = value
+    }
+
+  
+   
+    console.log(img_out)
+    
+  }
+
 };
 
 export default SimpleSwap;
 
-const cryptolist = [
-  { id: 1, icon: btc, value: "BTC" },
-  { id: 2, icon: btc, value: "BTC" },
-  { id: 3, icon: btc, value: "BTC" },
-  { id: 4, icon: btc, value: "BTC" },
-  { id: 5, icon: btc, value: "BTC" },
-];
-
-const ethlist = [
-  { id: 1, icon: eth, value: "ETH" },
-  { id: 2, icon: eth, value: "ETH" },
-  { id: 3, icon: eth, value: "ETH" },
-  { id: 4, icon: eth, value: "ETH" },
-  { id: 5, icon: eth, value: "ETH" },
-];
+let token_In
+let token_Out
 
 // const StyledModal = tw.div`
 //   flex
@@ -244,3 +264,10 @@ const ethlist = [
 //   min-h-min
 //   transform -translate-x-1/2 -translate-y-1/2
 //   `;
+
+
+const uniList = [
+  {value: "some", chainId: 3, address: "0x3346B2A939aA13e76Ce8Aa05ECCAe92E0D4F6580", symbol: "tETH", name: "tETH Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe", tags: ["stablecoin"]},
+  {value: "other", chainId: 3, address: "0x817F61606B7f073854c51ec93beF408708A5b4E4", symbol: "BTC", name: "BTC Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ", tags: ["stablecoin"]},
+  { }
+]
