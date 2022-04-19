254eb5a77bcab432e115942be4238b2f7db7b5ed
gamut-exchange <99559415+gamut-exchange@users.noreply.github.com>
2022-04-19 17:53:53 +0200

    addPools and Coin

--- src/config/constants.js
+++ src/config/constants.js
@@ -9,8 +9,9 @@ export const contractAddresses = {
 		'router': '0xbbd5D17B9aC782709724e16B3ABafB69a4913B09',
 		'hedgeFactory': '0xC19088c49DA26e8F74B28aDFD9D838ad495c3c19',
 		'btc': '0x1c63A8233786C1955724A676B87B6a4f5A1E0847',
-		'teth': '0x73B0aF7A379C0126671Db09d7fAf9b8122BAe02D',
-		'usdt': '0x7b4B44C987304dF194012d4Ae542629b7C2AC1D0',
+		'eth': '0x73B0aF7A379C0126671Db09d7fAf9b8122BAe02D',
+		'usdc': '0x7b4B44C987304dF194012d4Ae542629b7C2AC1D0',
+		'dai': '0x8CaA0072177ccEa341989F0Afd7cA5e6583390b6',
 	}
 }
 
@@ -22,8 +23,10 @@ export const uniList = {
 	],
 	'fantom': [
 	  {value: "btc", chainId: 4002, address: "0xE1F60643ED560a55b6aF90551c69Ec877DD105D9", symbol: "BTC", name: "BTC Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ", tags: ["stablecoin"]},
-	  {value: "usdt", chainId: 4002, address: "0x817F61606B7f073854c51ec93beF408708A5b4E4", symbol: "usdt", name: "USDT Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", tags: ["stablecoin"]},
-	  {value: "teth", chainId: 4002, address: "0x3346B2A939aA13e76Ce8Aa05ECCAe92E0D4F6580", symbol: "tETH", name: "tETH Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe", tags: ["stablecoin"]},
+	  {value: "usdc", chainId: 4002, address: "0x817F61606B7f073854c51ec93beF408708A5b4E4", symbol: "USDC", name: "USDC Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", tags: ["stablecoin"]},
+	  {value: "eth", chainId: 4002, address: "0x3346B2A939aA13e76Ce8Aa05ECCAe92E0D4F6580", symbol: "ETH", name: "ETH Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe", tags: ["stablecoin"]},
+	  {value: "dai", chainId: 4002, address: "0x8CaA0072177ccEa341989F0Afd7cA5e6583390b6", symbol: "DAI", name: "DAI Coin", decimals: 18, logoURL: "https://gateway.pinata.cloud/ipfs/QmWe2fZa2d3zbaXkmHF8qAn5RL52V9EdwL3nRxLHwLyGPN", tags: ["stablecoin"]},
+	
 	]
 }
 
@@ -33,7 +36,11 @@ export const poolList = {
 		{value: "other", address:"0x97edF4e1Aad15Ab44E4194b6E271E49d3E2e36c8", symbols:["USDT", "tETH"], logoURLs:["https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe"]}
 	],
 	'fantom': [
-		{value: "btc-teth", address:"0x4D72553001fE88371aEc189455E1Ed18849b8bA2", symbols:["BTC", "tETH"], logoURLs:["https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ", "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe"]},
-		{value: "usdt-teth", address:"0x97edF4e1Aad15Ab44E4194b6E271E49d3E2e36c8", symbols:["USDT", "tETH"], logoURLs:["https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe"]}
+		{value: "btc-eth", address:"0x4D72553001fE88371aEc189455E1Ed18849b8bA2", symbols:["BTC", "ETH"], logoURLs:["https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ", "https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe"]},
+		{value: "usdc-btc", address:"0x97edF4e1Aad15Ab44E4194b6E271E49d3E2e36c8", symbols:["USDT", "BTC"], logoURLs:["https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", "https://gateway.pinata.cloud/ipfs/QmZ5Fu4uwjENpbLRGs1eDrYHM5JMXQjSD3gkAi8bW63riJ"]},
+		{value: "eth-dai", address:"0x758E5f4caeD36BF1d0Bb23C31387cfF11498D16D", symbols:["ETH", "DAI"], logoURLs:["https://gateway.pinata.cloud/ipfs/QmXtWNMhcz6myKn2acQLvhfSzUc3CEB17uTaS8NKSm9fPe", "https://gateway.pinata.cloud/ipfs/QmWe2fZa2d3zbaXkmHF8qAn5RL52V9EdwL3nRxLHwLyGPN"]},
+		{value: "usdc-dai", address:"0xC604a85346523d4e66aBF9c215CFfb13dd154286", symbols:["USDC", "DAI"], logoURLs:["https://gateway.pinata.cloud/ipfs/Qmf7wTW9iJCKbY8XTq8KQjMhXT8GGoSuA3hjfkKqEwBshV", "https://gateway.pinata.cloud/ipfs/QmWe2fZa2d3zbaXkmHF8qAn5RL52V9EdwL3nRxLHwLyGPN"]}
+	
+
 	]
 }
